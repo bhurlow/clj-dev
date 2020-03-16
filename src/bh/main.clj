ed53a76d208fd85980c6b0f24e4de293cbb0bf21
Brian Hurlow <brian@brianhurlow.com>
2020-03-16 12:39:58 -0400

    support port opt

--- src/bh/main.clj
+++ src/bh/main.clj
@@ -1,19 +1,22 @@
 (ns bh.main
   (:require [cider.nrepl :refer (cider-nrepl-handler)]
-            [nrepl.server :refer (start-server stop-server)]
+            [nrepl.server :refer (start-server)]
             [rebel-readline.main]))
 
 
 (defonce nrepl-server (atom nil))
 
-(defn start-nrepl! []
- (spit ".nrepl-port" 7888)
+
+(defn start-nrepl! [port]
+ (spit ".nrepl-port" port)
  (reset! nrepl-server
-         (start-server :port 7888
+         (start-server :port port
                        :handler cider-nrepl-handler)))
 
-(defn -main []
-  (println "local dev")
-  (start-nrepl!)
-  (rebel-readline.main/-main))
+(defn -main 
+  ([] (-main 7888))
+  ([port]
+   (println "local dev")
+   (start-nrepl! (Integer. port))
+   (rebel-readline.main/-main)))
 
