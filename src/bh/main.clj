af712afc46959bf90f83dee2b0e0cbf27450626f
Brian Hurlow <brian@brianhurlow.com>
2020-04-15 10:21:04 -0400

    patch rebel readline to handle interrupts

--- src/bh/main.clj
+++ src/bh/main.clj
@@ -1,7 +1,9 @@
 (ns bh.main
   (:require [cider.nrepl :refer (cider-nrepl-handler)]
+            [clojure.repl :as clj-repl]
             [nrepl.server :refer (start-server)]
-            [rebel-readline.main]))
+            [rebel-readline.core :as rebel-core]
+            [rebel-readline.clojure.main :as rebel-clj-main]))
 
 
 (defonce nrepl-server (atom nil))
@@ -13,10 +15,33 @@
          (start-server :port port
                        :handler cider-nrepl-handler)))
 
+
+(defn -handle-sigint-form
+  []
+  `(let [thread# (Thread/currentThread)]
+     (clj-repl/set-break-handler! (fn [_signal#] (.stop thread#)))))
+
+(defn start-repl!
+  [& [init-ns]]
+  (rebel-core/ensure-terminal
+    (rebel-clj-main/repl*
+      {:init (fn []
+               ; HACK: rebel-readline doesn't have a convenient way to change init ns (it's always `user`).
+               ; See https://github.com/bhauman/rebel-readline/issues/157.
+               (when (some? init-ns)
+                 (let [init-ns (symbol init-ns)]
+                   (require init-ns)
+                   (in-ns init-ns))))
+
+       :eval (fn [form]
+               ; HACK: allows Ctrl+C to interrupt long running tasks.
+               ; See https://github.com/bhauman/rebel-readline/issues/180#issuecomment-429057767.
+               (eval `(do ~(-handle-sigint-form) ~form)))})))
+
 (defn -main 
   ([] (-main 7888))
   ([port]
    (println "local dev")
    (start-nrepl! (Integer. port))
-   (rebel-readline.main/-main)))
+   (start-repl! 'user.main)))
 
